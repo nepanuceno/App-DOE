74aebd017222989b828681e4c116fd2dc5b4f229
Paulo Roberto Torres <paulo.torres.apps@gmail.com>
2023-09-25 15:33:06 -0300

    Refresh ao deslizar o dedo de cima para baixo na tela principal

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,28 +8,32 @@ const Home = ({ navigation }) => {
     
     const [objDiariosLista, setObjDiarios] = useState({});
 
+    const getDiarios = () => {
+        const dados = useGetDiarios(baseUrl).then((response:object) => {
+            if(response.status==200) {
+                console.log("VEio")
+                setObjDiarios(response.data);
+            } else {
+                Alert.alert("ERRO!",response.message);
+            }
+        }).catch((err) => {
+            console.log("ERRO GRAVE",err.toString())
+            if (err.request._response != null) {
+                Alert.alert("Erro ao Consultar", err.request._response);
+            } else {
+                Alert.alert("Erro Grave", "Ocorreu um erro ao consultar. Por favor, tente mais tarde.");
+            }
+        });
+    }
+
     useEffect( () => {
-        console.log("Refresh")
-        const dados = useGetDiarios(baseUrl)
-            .then((response:object) => {
-                if(response.status==200) {
-                    setObjDiarios(response.data);
-                } else {
-                    Alert.alert("ERRO!",response.message);
-                }
-            }).catch((err) => {
-                console.log("ERRO GRAVE",err.toString())
-                if (err.request._response != null) {
-                    Alert.alert("Erro ao Consultar", err.request._response);
-                } else {
-                    Alert.alert("Erro ao Grave", "Ocorreu um erro ao consultar. Por favor, tente mais tarde.");
-                }
-            });
+        console.log("Refresh");
+        getDiarios();
     }, []);
     
     return (
         <SafeAreaView>
-            <ListaDiarios dados={objDiariosLista} navigation={navigation}/>
+            <ListaDiarios dados={objDiariosLista} navigation={navigation} childToParent={getDiarios} />
         </SafeAreaView>
     )
 };
--- src/components/ListaDiarios/index.tsx
+++ src/components/ListaDiarios/index.tsx
@@ -1,31 +1,53 @@
-import { FlatList, View } from "react-native";
+import { FlatList, RefreshControl, View } from "react-native";
 import Diario from "../Diario";
+import { useEffect, useState } from "react";
 interface Params {
     dados: object
     navigation: object
+    childToParent
 };
 
+
 const ListaDiarios = ( props: Params ) => {
-    const dados = props.dados
+    const [refreshing, setRefresh] = useState(false);
+
+    const onRefresh = () => {
+        setRefresh(true);
+        props.childToParent()
+    }
+
+    const dados = props.dados;
+
+    useEffect(() => {
+        console.log("FEz Refresh")
+        setRefresh(false);
+    }, [dados]);
+
     return (
         <FlatList 
             data={dados}
             renderItem={({item}) => (
-            <View>
-                <Diario
-                    id={item.id} 
-                    edicao={item.edicao} 
-                    data={item.data_publicacao ? item.data_publicacao : item.data } 
-                    suplemento={item.suplemento} 
-                    paginas={item.paginas} 
-                    tamanho={item.tamanho} 
-                    downloads={item.downloads} 
-                    link={item.link} 
-                    imagem={item.imagem}
-                    navigation={props.navigation} 
+                <View>
+                    <Diario
+                        id={item.id}
+                        edicao={item.edicao}
+                        data={item.data_publicacao ? item.data_publicacao : item.data }
+                        suplemento={item.suplemento}
+                        paginas={item.paginas} 
+                        tamanho={item.tamanho}
+                        downloads={item.downloads}
+                        link={item.link}
+                        imagem={item.imagem}
+                        navigation={props.navigation}
+                    />
+                </View>
+            )}
+            refreshControl={
+                <RefreshControl
+                  refreshing={refreshing}
+                  onRefresh={onRefresh}
                 />
-            </View>
-        )}
+            }
         />
     );
 }
